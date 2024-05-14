3a180f87b26862d338190c065022b84b2fd03001
manlikeclaro <ckibii08@gmail.com>
2024-05-14 14:54:13 +0300

    [FEAT] implemented post, put & del functions on views

--- watchlist/api/serializers.py
+++ watchlist/api/serializers.py
@@ -1,8 +1,21 @@
 from rest_framework import serializers
 
+from watchlist.models import Movie
+
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     is_active = serializers.BooleanField()
+
+    def create(self, validated_data):
+        movie = Movie.objects.create(**validated_data)
+        return movie
+
+    def update(self, instance, validated_data):
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.is_active = validated_data.get('is_active', instance.is_active)
+        instance.save()
+        return instance
--- watchlist/api/views.py
+++ watchlist/api/views.py
@@ -12,6 +12,15 @@ class MoviesView(APIView):
         serializer = MovieSerializer(self.movies, many=True)
         return Response(serializer.data, 200, )
 
+    def post(self, request):
+        serializer = MovieSerializer(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, 400, )
+
+        serializer.save()
+        return Response(serializer.data, 201, )
+
 
 class SingleMovieView(APIView):
     movies = Movie.objects.all()
@@ -19,3 +28,18 @@ class SingleMovieView(APIView):
     def get(self, request, movie_id):
         serializer = MovieSerializer(self.movies.get(pk=movie_id))
         return Response(serializer.data, 200, )
+
+    def put(self, request, movie_id):
+        movie = self.movies.get(pk=movie_id)
+        serializer = MovieSerializer(movie, data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, 400, )
+
+        serializer.save()
+        return Response(serializer.data, 201, )
+
+    def delete(self, request, movie_id):
+        movie = self.movies.get(pk=movie_id)
+        movie.delete()
+        return Response(status=204)
